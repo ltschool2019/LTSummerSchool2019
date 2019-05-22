6661ce95422853328bdeca15c124d2cede9fd0d8
alexander.moskvin <mav_92@mail.ru>
2019-05-22 15:43:41 +0300

    Demo project

---
+++ LTTimeRegistrator/Migrations/20190518091841_Initial.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LTTimeRegistrator.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Surname = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ LTTimeRegistrator/Models/ApplicationContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LTTimeRegistrator.Models
+{
+  /// <summary>
+  /// 
+  /// </summary>
+  public class ApplicationContext : DbContext
+  {
+    public ApplicationContext(DbContextOptions<ApplicationContext> options)
+      : base(options) { }
+
+    public DbSet<User> Users { get; set; }
+  }
+}
