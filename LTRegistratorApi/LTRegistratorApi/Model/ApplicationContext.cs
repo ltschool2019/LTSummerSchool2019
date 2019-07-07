13eb3d74b8039b1a00ac4971c5e8dad52529e591
IlinykhYE <47817626+IlinykhYE@users.noreply.github.com>
2019-07-07 21:03:21 +0300

    Yi authendefication (#2)

--- LTRegistratorApi/LTRegistratorApi/Model/ApplicationContext.cs
+++ LTRegistratorApi/LTRegistratorApi/Model/ApplicationContext.cs
@@ -1,18 +1,22 @@
 ﻿using LTRegistratorApi.Model;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace LTTimeRegistrator.Models
 {
-  /// <summary>
-  /// 
-  /// </summary>
-  public class ApplicationContext : DbContext
-  {
-    public ApplicationContext(DbContextOptions<ApplicationContext> options)
-      : base(options)
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ApplicationContext : IdentityDbContext<IdentityUser>
     {
-    }
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+          : base(options)
+        {
+        }
 
-    public DbSet<Value> Values { get; set; }
-  }
+        public DbSet<Value> Values { get; set; }
+
+
+    }
 }
