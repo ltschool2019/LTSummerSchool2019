50bab75576b3ea027265b9317037eb652b195aa8
AnnaStepakova <anstepakova@yandex.ru>
2019-07-16 12:47:54 +0300

    разделила на компоненты

--- LTRegistratorUI/src/app/app.module.ts
+++ LTRegistratorUI/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { SideMenuComponent } from './side-menu/side-menu.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeComponent,
+    SideMenuComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
