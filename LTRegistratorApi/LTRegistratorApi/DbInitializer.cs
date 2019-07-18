5bc86560448a21f2698d1c0c546e549cf32498cc
KostinP <st069165@student.spbu.ru>
2019-07-18 10:44:02 +0300

    Resolve Conflicts

--- LTRegistratorApi/LTRegistratorApi/DbInitializer.cs
+++ LTRegistratorApi/LTRegistratorApi/DbInitializer.cs
@@ -10,65 +10,72 @@ using System.Threading.Tasks;
 
 namespace LTRegistratorApi
 {
+    /// <summary>
+    /// Adding values ​​to a table.
+    /// </summary>
     public class DbInitializer
     {
         public static void Initialize(ApplicationContext context, UserManager<ApplicationUser> userManager)
         {
             context.Database.EnsureCreated();
 
-            var users = new RegisterDto[]
+            if (!context.Employee.Any())
             {
-                new RegisterDto { Name = "Alice", Email = "alice@mail.ru", Password = "aA123456!", Role = "Administrator" },
-                new RegisterDto { Name = "Bob", Email = "b0b@yandex.ru", Password = "+B0o0B+", Role = "Manager" },
-                new RegisterDto { Name = "Eve", Email = "eve.99@yandex.ru", Password = "1Adam!!!", Role = "Employee" },
-                new RegisterDto { Name = "Carol", Email = "car0l@mail.ru", Password = "+C0o0C+", Role = "Manager" },
-                new RegisterDto { Name = "Dave", Email = "dave.99@mail.ru", Password = "1Adam!!!", Role = "Employee" },
-                new RegisterDto { Name = "Frank", Email = "frank.99@mail.ru", Password = "1Adam!!!", Role = "Employee" }
-            };
+                var leaveEve = new Leave[]
+                {
+                    new Leave() { StartDate = new DateTime(2019, 1, 1), EndDate = new DateTime(2019, 1, 13), TypeLeave = TypeLeave.Vacation },
+                    new Leave() { StartDate = new DateTime(2019, 2, 10), EndDate = new DateTime(2019, 2, 13), TypeLeave = TypeLeave.SickLeave }
+                };
+                var leaveCarol = new Leave[]
+                {
+                    new Leave() { StartDate = new DateTime(2019, 1, 1), EndDate = new DateTime(2019, 1, 13), TypeLeave = TypeLeave.Vacation },
+                    new Leave() { StartDate = new DateTime(2019, 3, 1), EndDate = new DateTime(2019, 4, 1), TypeLeave = TypeLeave.SickLeave }
+                };
+                var leaveFrank = new Leave[]
+                {
+                    new Leave() { StartDate = new DateTime(2019, 1, 1), EndDate = new DateTime(2019, 1, 13), TypeLeave = TypeLeave.Vacation }
+                };
+                context.Employee.Add(new Employee() { FirstName = "Alice", SecondName = "Brown", Mail = "alice@mail.ru", MaxRole = "Administrator" });
+                context.Employee.Add(new Employee() { FirstName = "Bob", SecondName = "Johnson", Mail = "b0b@yandex.ru", MaxRole = "Manager", Leave = leaveEve });
+                context.Employee.Add(new Employee() { FirstName = "Eve", SecondName = "Williams", Mail = "eve.99@yandex.ru", MaxRole = "Employee" });
+                context.Employee.Add(new Employee() { FirstName = "Carol", SecondName = "Peterson", Mail = "car0l@mail.ru", MaxRole = "Manager", Leave = leaveCarol });
+                context.Employee.Add(new Employee() { FirstName = "Dave", SecondName = "Finley", Mail = "dave.99@mail.ru", MaxRole = "Employee"});
+                context.Employee.Add(new Employee() { FirstName = "Frank", SecondName = "Florence", Mail = "frank.99@mail.ru", MaxRole = "Employee", Leave = leaveFrank });
 
-            if (context.Users.Count() == 0)
-            {
-                foreach (var model in users)
+                context.SaveChanges();
+
+                foreach (var employee in context.Employee)
                 {
                     var user = new ApplicationUser
                     {
-                        UserName = model.Email, //for PasswordSignInAsync
-                        Email = model.Email,
+                        UserName = employee.Mail, //for PasswordSignInAsync
+                        Email = employee.Mail,
+                        EmployeeId = employee.EmployeeId
                     };
 
-                    var result = userManager.CreateAsync(user, model.Password).Result;
-                    var resultAddRole = userManager.AddClaimAsync(user, new Claim(ClaimTypes.Role, model.Role)).Result;
-                    var resultAddName = userManager.AddClaimAsync(user, new Claim(ClaimTypes.Name, model.Name)).Result;
-
-                    if (!(result.Succeeded && resultAddRole.Succeeded && resultAddName.Succeeded))
+                    var result = userManager.CreateAsync(user, employee.Mail + "Password1").Result;
+                    var resultAddRole = userManager.AddClaimAsync(user, new Claim(ClaimTypes.Role, employee.MaxRole)).Result;
+                    if (!(result.Succeeded && resultAddRole.Succeeded))
                         throw new ApplicationException("ERROR_INITIALIZE_DB");
                 }
             }
 
-            var leaveEve = new Leave[]
+            if (!context.Project.Any())
             {
-                new Leave() { StartDate = new DateTime(2019, 1, 1), EndDate = new DateTime(2019, 1, 13), TypeLeave = TypeLeave.Vacation },
-                new Leave() { StartDate = new DateTime(2019, 2, 10), EndDate = new DateTime(2019, 2, 13), TypeLeave = TypeLeave.SickLeave }
-            };
-            var leaveCarol = new Leave[]
-            {
-                new Leave() { StartDate = new DateTime(2019, 1, 1), EndDate = new DateTime(2019, 1, 13), TypeLeave = TypeLeave.Vacation },
-                new Leave() { StartDate = new DateTime(2019, 3, 1), EndDate = new DateTime(2019, 4, 1), TypeLeave = TypeLeave.SickLeave }
-            };
-            var leaveFrank = new Leave[]
-            {
-                new Leave() { StartDate = new DateTime(2019, 1, 1), EndDate = new DateTime(2019, 1, 13), TypeLeave = TypeLeave.Vacation }
-            };
+                context.Project.Add(new Project() { Name = "A" });
+                context.Project.Add(new Project() { Name = "B" });
+                context.Project.Add(new Project() { Name = "С" });
+                context.SaveChanges();
+            }
 
-            if (!context.Employee.Any())
+            if (!context.ProjectEmployee.Any())
             {
-                context.Employee.Add(new Employee() { User = "Alice" });
-                context.Employee.Add(new Employee() { User = "Eve", Leave = leaveEve });
-                context.Employee.Add(new Employee() { User = "Bob" });
-                context.Employee.Add(new Employee() { User = "Carol", Leave = leaveCarol });
-                context.Employee.Add(new Employee() { User = "Dave" });
-                context.Employee.Add(new Employee() { User = "Frank", Leave = leaveFrank });
-
+                context.ProjectEmployee.Add(new ProjectEmployee() { ProjectId = 1, EmployeeId = 1, Role = "Manager" });
+                context.ProjectEmployee.Add(new ProjectEmployee() { ProjectId = 3, EmployeeId = 1, Role = "Manager" });
+                context.ProjectEmployee.Add(new ProjectEmployee() { ProjectId = 2, EmployeeId = 1, Role = "Employee" });
+                context.ProjectEmployee.Add(new ProjectEmployee() { ProjectId = 1, EmployeeId = 2, Role = "Employee" });
+                context.ProjectEmployee.Add(new ProjectEmployee() { ProjectId = 2, EmployeeId = 2, Role = "Employee" });
+                context.ProjectEmployee.Add(new ProjectEmployee() { ProjectId = 1, EmployeeId = 3, Role = "Employee" });
                 context.SaveChanges();
             }
         }
--- LTRegistratorApi/LTRegistratorApi/Model/Employee.cs
+++ LTRegistratorApi/LTRegistratorApi/Model/Employee.cs
@@ -9,10 +9,12 @@ namespace LTRegistratorApi.Model
     public class Employee
     {
         public int EmployeeId { get; set; }
-        public string UserId { get; set; }
-        public string User { get; set; }
-        public ICollection<ProjectEmployee> ProjectEmployee { get; set; }
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public string Mail { get; set; }
+        public string MaxRole { get; set; }
 
+        public ICollection<ProjectEmployee> ProjectEmployee { get; set; }
         public virtual ApplicationUser ApplicationUser { get; set; }
         public ICollection<Leave> Leave { get; set; }
     }
