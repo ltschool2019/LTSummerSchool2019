cb770f2b2e5e15f070b74d018014de798b260796
AnnaStepakova <51366746+AnnaStepakova@users.noreply.github.com>
2019-07-19 21:17:53 +0300

    Employee start page (#8)
    добавлена страница проставления часов и компоненты хедера и меню

--- LTRegistratorUI/src/app/app.module.ts
+++ LTRegistratorUI/src/app/app.module.ts
@@ -2,11 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+
+import { EmployeeComponent } from './employee/employee.component';
+import { SideMenuComponent } from './side-menu/side-menu.component';
+import { HeaderComponent } from './header/header.component';
 import { LoginComponent } from './login/login.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    EmployeeComponent,
+    SideMenuComponent,
+    HeaderComponent,
     LoginComponent
   ],
   imports: [
