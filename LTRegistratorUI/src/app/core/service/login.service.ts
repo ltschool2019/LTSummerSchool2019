42687f64d0ce86de6ae6873f7107b59eb14ff4c6
gulyashik <guliaeva.olya2014@yandex.ru>
2019-07-20 16:47:56 +0300

    add token to localstorage

--- LTRegistratorUI/src/app/core/service/login.service.ts
+++ LTRegistratorUI/src/app/core/service/login.service.ts
@@ -3,6 +3,8 @@ import {HttpClient} from "@angular/common/http";
 import {Observable} from 'rxjs';
 import {User} from "../../shared/models/user.model";
 import { map } from 'rxjs/operators';
+import * as moment from "moment";
+
 
 @Injectable()
 export class LoginService {
@@ -10,9 +12,15 @@ export class LoginService {
   public token_Json = new String;
   constructor(private http: HttpClient) {}
   getUser(email:string, password:string) {
-    return this.http.post<string>('http://localhost:53634/api/account/login', {email, password})
+    return this.http.post<any>('http://localhost:53634/api/account/login', {email, password}).pipe(map(res => this.setSession) )
     // return this.http.get('http://localhost:3000/token')
     // .pipe(catchError(this.handleError('login', hero))
     // );
   }
+  private setSession(authResult) {
+    const expiresAt = moment().add(authResult.expiresIn,'second');
+
+    localStorage.setItem('id_token', authResult.idToken);
+    localStorage.setItem("expires_at", JSON.stringify(expiresAt.valueOf()) );
+}          
 }
