b1e2ea5c8dde346878ca2fa99365812bac0b0616
gulyashik <guliaeva.olya2014@yandex.ru>
2019-07-21 23:52:40 +0300

    add interceptor

--- LTRegistratorUI/src/app/app.module.ts
+++ LTRegistratorUI/src/app/app.module.ts
@@ -7,9 +7,11 @@ import { EmployeeComponent } from './employee/employee.component';
 import { SideMenuComponent } from './side-menu/side-menu.component';
 import { HeaderComponent } from './header/header.component';
 import { LoginComponent } from './login/login.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS  } from '@angular/common/http';
 import { LoginService } from 'src/app/core/service/login.service';
 import {FormsModule} from '@angular/forms';
+import { JwtInterceptor} from 'src/app/helpers/jwt_interceptors.service';
+
 
 
 @NgModule({
@@ -27,6 +29,7 @@ import {FormsModule} from '@angular/forms';
   ],
   providers: [
     LoginService,
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
 
   ],
   bootstrap: [AppComponent]
