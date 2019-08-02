fd53346c601dce2504355f84b0efdfc16f0bf6e6
gulyashik <guliaeva.olya2014@yandex.ru>
2019-08-02 16:45:29 +0300

    add material-design to package.json and editing model  manager_projects.model

--- LTRegistratorUI/src/app/shared/models/manager_projects.model.ts
+++ LTRegistratorUI/src/app/shared/models/manager_projects.model.ts
@@ -2,8 +2,9 @@ export class ManagerProjects {
     name: string;
     employeeId: number;
     projects: [];
+    projects_id: [];
 
-    constructor(name: string, id: number, projects: []) {
+    constructor(name: string, id: number, projects: [], projects_id: []) {
         this.name = name;
         this.employeeId = id;
         this.projects = projects;
