27a8c0c58024d8db3e5397efca98aba1d59a6f5b
gulyashik <guliaeva.olya2014@yandex.ru>
2019-08-04 22:25:39 +0300

    delete comments

--- LTRegistratorUI/src/app/core/service/manager_projects.service.ts
+++ LTRegistratorUI/src/app/core/service/manager_projects.service.ts
@@ -12,25 +12,12 @@ import { ManagerProjects } from 'src/app/shared/models/manager_projects.model';
 export class ManagerProjectsService {
   private id = 2;
   private manager_projectsUrl = `http://localhost:53635/api/manager/${this.id}/projects`;
-  public man_project: ManagerProjects[] ;
 
   constructor(
     private http: HttpClient
   ) { }
 
-
-  // getManagerProjects() {
-  //   return this.http.get<any>('http://localhost:53635/api/manager/2/projects')
-  //   .subscribe((model: ManagerProjects[]) => this.setModelManagerProjets(model));
-  // }
-  // setModelManagerProjets(model) {
-  //   this.man_project = model;
-  // }
   getManagerProjects():any {
-    // return this.http.get<ManagerProjects[]>(this.manager_projectsUrl).pipe(map(data => {
-    //   debugger;
-    //   return data;
-    // }));
     return this.http.get<any>(this.manager_projectsUrl);
 
   }
