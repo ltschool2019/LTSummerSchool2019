84122d30315fd7f7665e902b6c6eb5d2effa766b
gulyashik <guliaeva.olya2014@yandex.ru>
2019-08-04 22:21:13 +0300

    add design of manager-projects page and routing for manager-projects

--- LTRegistratorUI/src/app/manager-projects/manager-projects.component.ts
+++ LTRegistratorUI/src/app/manager-projects/manager-projects.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { ManagerProjectsService } from 'src/app/core/service/manager_projects.service';
 import { ManagerProjects } from 'src/app/shared/models/manager_projects.model';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { MaterialModule } from "src/app/material.module";
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-manager-projects',
@@ -8,17 +14,22 @@ import { ManagerProjects } from 'src/app/shared/models/manager_projects.model';
   styleUrls: ['./manager-projects.component.scss']
 })
 export class ManagerProjectsComponent implements OnInit {
-  public man_project: ManagerProjects[] ;
+  public man_project: MatTableDataSource<ManagerProjects[]> ;
+  manProjectForm: FormGroup;
+  displayedColumns: string[] = ['name', 'status', 'export','delete'];
 
   constructor(
-    private managerProjectsService: ManagerProjectsService) { }
+    private managerProjectsService: ManagerProjectsService) { 
+    }
 
   ngOnInit() {
     this.getManagerProjects();
   }
+  
   getManagerProjects(): void {
     this.managerProjectsService.getManagerProjects()
-    .subscribe((data) =>{this.man_project = data});
+    .subscribe((data) =>{this.man_project =  data});
+   // .subscribe((data) =>{this.man_project = data});
     /* this.vacationService.getVacations().subscribe((vacations: Vacation[]) => {
        this.vacations = vacations.map(
          (vacation: any) =>
