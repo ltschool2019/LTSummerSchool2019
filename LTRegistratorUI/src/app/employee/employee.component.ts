47e05d1c009964a1ded0db3d55bd08d973f80eae
TheJuze <dimanikus0106@gmail.com>
2019-08-11 15:23:17 +0300

    предварительная верстка

--- LTRegistratorUI/src/app/employee/employee.component.ts
+++ LTRegistratorUI/src/app/employee/employee.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
+export interface Project {
+  value: string;
+  viewValue: string;
+}
 @Component({
   selector: 'app-employee',
   templateUrl: './employee.component.html',
@@ -7,6 +10,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeeComponent implements OnInit {
   days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+  projects: Project[] = [//FIXME: получать с сервера
+    { value: 'steak-0', viewValue: 'Steak' },
+    { value: 'pizza-1', viewValue: 'Pizza' },
+    { value: 'tacos-2', viewValue: 'Tacos' }
+  ];
   constructor() { }
 
   ngOnInit() {
