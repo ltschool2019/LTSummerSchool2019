bfd699bcfda62153a95bf57ac146446b5fc6939e
TheJuze <dimanikus0106@gmail.com>
2019-08-17 12:43:01 +0300

    Изменил календарь, добавил кнопки смены недели
    *все еще нужна работа

--- LTRegistratorUI/src/app/employee/employee.component.ts
+++ LTRegistratorUI/src/app/employee/employee.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+//import { DateAdapter } from '@angular/material';
 
 import { UserService } from '../core/service/user.service';
 import { Project } from '../core/models/project.model';
@@ -20,29 +21,53 @@ export class EmployeeComponent implements OnInit {
   week: Day[] = [];
   getWeek() {
     //FIXME: ночью неверные значения
-    this.week =[];
+    this.week = [];
     let curr = new Date(this.taskForm.get('currentWeek').value);
+    console.log('curr=' + curr);
     for (let i = 1; i <= 7; i++) {//getDate() - Получить число месяца, от 1 до 31.
       let first = curr.getDate() - curr.getDay() + i;//getDay() - Получить номер дня в неделе.(от 0(вс) до 6 (сб)). В итоге получаем дни с пн по вс
-      let day = new Date(curr.setDate(first)).toISOString().slice(8, 10);//преобразуем и получаем только число
+
+      console.log('getDate=' + curr.getDate());
+      console.log('getDay' + curr.getDay());
+      curr.setDate(first);//преобразуем и получаем только число
+      let day = curr.getDate();
+      console.log('curr=' + curr);
       let newDay = new Day(+day, i);
       this.week.push(newDay);//добавить в массив дней недели
     }
+    console.log('___________________________________________-');
   }
   projects: Project[];
   constructor(
     private fb: FormBuilder,
-    private userService: UserService
+    private userService: UserService,
+  ///  private dateAdapter: DateAdapter<any>
   ) { }
   previousWeek() {//выбранный день -7 назад
-
+    this.week = [];
+    let curr = new Date(this.taskForm.get('currentWeek').value);
+    let first = curr.getDate() - 7;
+    let day = new Date(curr.setDate(first));
+    this.taskForm.patchValue({
+      currentWeek: day
+    })
+    this.getWeek();
   }
   nextWeek() {//выбранный +7 вперед
+    this.week = [];
+    let curr = new Date(this.taskForm.get('currentWeek').value);
+    let first = curr.getDate() + 7;
+    let day = new Date(curr.setDate(first));
+    this.taskForm.patchValue({
+      currentWeek: day
+    })
+    this.getWeek();
 
   }
   ngOnInit() {
     this.getProject();
     this.initForm();
+    //this.dateAdapter.setLocale('ru-Latn');
     this.getWeek();
   }
   getProject() {
