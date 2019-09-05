5417d981cf844268486066558a39c27194b8cdd3
Anton <anvitsapik@gmail.com>
2019-09-05 13:49:21 +0700

    Monthly report (#73)
    * add report
    
    * fix autofitcolums
    
    * fix norm hours, code style
    
    * comments
    
    * fix  auth

---
+++ LTRegistratorApi/LTRegistrator.BLL.Contracts/Models/HourReportBllModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LTRegistrator.BLL.Contracts.Models
+{
+    public class HourReportBllModel
+    {
+        public IEnumerable<HourReportUserBllModel> Users { get; set; }
+        public IEnumerable<Event> Events { get; set; }
+    }
+
+    public class HourReportUserBllModel
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public string LastName { get; set; }
+        public double Rate { get; set; }
+        public double NormHours { get; set; }
+        public IEnumerable<HourReportProjectBllModel> Projects { get; set; }
+        public IEnumerable<HourReportLeaveBllModel> Leaves { get; set; }
+    }
+
+    public class HourReportProjectBllModel
+    {
+        public int ProjectId { get; set; }
+        public string ProjectName { get; set; }
+        public double Hours { get; set; }
+        public int ManagerId { get; set; }
+    }
+
+    public class HourReportLeaveBllModel
+    {
+        public EventType EventType { get; set; }
+        public string Name { get; set; }
+        public double Hours { get; set; }
+    }
+
+    public class Event
+    {
+        public string Name { get; set; }
+        public EventType EventType { get; set; }
+    }
+
+    public enum EventType
+    {
+        ManagerProject,
+        Vacation,
+        AnotherLeave,
+        NotManagerProject
+    }
+}
---
+++ LTRegistratorApi/LTRegistrator.BLL.Services/Mappings/DataMappingProfile.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AutoMapper;
+using LTRegistrator.BLL.Contracts.Models;
+using LTRegistrator.Domain.Entities;
+
+namespace LTRegistrator.BLL.Services.Mappings
+{
+    public class DataMappingProfile : Profile
+    {
+        public DataMappingProfile()
+        {
+            #region HourReport
+
+            CreateMap<IEnumerable<Employee>, HourReportBllModel>()
+                .AfterMap((src, dest) =>
+                {
+                    var leaves = src.SelectMany(e => e.Leaves).GroupBy(l => l.TypeLeave).Select(l => l.Key).Select(tl => new Event
+                    {
+                        Name = EnumAssociations.LeaveNames[tl],
+                        EventType = EnumAssociations.LeaveEventTypes[tl]
+                    });
+
+                    var manager = src.FirstOrDefault()?.Manager;
+                    var managerProjects = new List<int>();
+                    if (manager != null)
+                        managerProjects = manager.ProjectEmployees.Select(pem => pem.ProjectId).ToList();
+                    var projects = src.SelectMany(e => e.ProjectEmployees).Select(pe => new Event
+                    {
+                        Name = pe.Project.Name,
+                        EventType = managerProjects.Contains(pe.ProjectId)
+                            ? EventType.ManagerProject
+                            : EventType.NotManagerProject
+                    }).GroupBy(p => new { p.EventType, p.Name }).Select(g => g.First());
+                    var result = leaves.ToList();
+                    result.AddRange(projects);
+                    dest.Events = result.OrderBy(e => e.EventType).ToList();
+                })
+                .ForMember(hr => hr.Users, opt => opt.MapFrom(src => src))
+                .ForAllOtherMembers(opt => opt.Ignore());
+
+            CreateMap<Employee, HourReportUserBllModel>()
+                .AfterMap((src, dest) =>
+                {
+                    var leaves = src.Leaves.GroupBy(l => l.TypeLeave).Select(g => new HourReportLeaveBllModel
+                    {
+                        EventType = EnumAssociations.LeaveEventTypes[g.Key],
+                        Name = EnumAssociations.LeaveNames[g.Key],
+                        Hours = g.Sum(l => l.EndDate.Month == l.StartDate.Month
+                            ? (l.EndDate - l.StartDate).TotalDays * 8
+                            : ((l.EndDate - l.StartDate).TotalDays - l.EndDate.Day) * 8)
+                    }).ToList();
+                    dest.Leaves = leaves;
+                })
+                .ForMember(hru => hru.UserId, opt => opt.MapFrom(src => src.Id))
+                .ForMember(hru => hru.FirstName, opt => opt.MapFrom(src => src.FirstName))
+                .ForMember(hru => hru.Surname, opt => opt.MapFrom(src => src.SecondName))
+                .ForMember(hru => hru.Rate, opt => opt.MapFrom(src => src.Rate))
+                .ForMember(hru => hru.Projects, opt => opt.MapFrom(src => src.ProjectEmployees))
+                .ForMember(hru => hru.NormHours, opt => opt.MapFrom((src, dist, res, context) =>
+                {
+                    var workingHoursInMonth = (int)context.Items["workingHoursInMonth"];
+                    return workingHoursInMonth * src.Rate;
+                }))
+                .ForAllOtherMembers(opt => opt.Ignore());
+
+            CreateMap<int, HourReportUserBllModel>()
+                .ForMember(hru => hru.NormHours, opt => opt.MapFrom((src, crr) => crr.Rate * src))
+                .ForAllOtherMembers(opt => opt.Ignore());
+
+            CreateMap<ProjectEmployee, HourReportProjectBllModel>()
+                .ForMember(hrp => hrp.ProjectId, opt => opt.MapFrom(src => src.ProjectId))
+                .ForMember(hrp => hrp.ProjectName, opt => opt.MapFrom(src => src.Project.Name))
+                .ForMember(hrp => hrp.Hours, opt => opt.MapFrom(src => src.Tasks.Sum(t => t.TaskNotes.Sum(tn => tn.Hours))))
+                .ForAllOtherMembers(opt => opt.Ignore());
+
+            #endregion
+        }
+    }
+}
